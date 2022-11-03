23a55489ce087364ea1e1f9a4724d30dc5f3bae8
shini161 <85759040+shini161@users.noreply.github.com>
2022-11-03 21:10:16 +0100

    updated VariableValues type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,7 @@ export type BinaryNumber = 0 | 1;
 
 export type BoolBinary = BinaryNumber | boolean;
 
-export type VariableValues = { [key: string]: BinaryNumber };
+export type VariableValues = { [key: string]: BoolBinary };
 
 export type EveryCaseReturnData = { [key: string]: BinaryNumber; res: BinaryNumber }
 
@@ -16,3 +16,29 @@ export interface EveryCase {
     res: BinaryNumber[];
     data: EveryCaseReturnData[]
 }
+
+/*
+declare function everyCase(pattern: string): EveryCase;
+
+declare function singleCase(pattern: string, variables?: VariableValues): SingleCase;
+
+declare function and(...args: BoolBinary[]): BinaryNumber;
+
+declare function buffer(number: BoolBinary): BinaryNumber;
+
+declare function nand(...args: BoolBinary[]): BinaryNumber;
+
+declare function nor(...args: BoolBinary[]): BinaryNumber;
+
+declare function not(number: BoolBinary): BinaryNumber;
+
+declare function or(...args: BoolBinary[]): BinaryNumber;
+
+declare function xnor(...args: BoolBinary[]): BinaryNumber;
+
+declare function xor(...args: BoolBinary[]): BinaryNumber;
+
+declare function checkPattern(pattern: string, variables: VariableValues): string;
+
+declare function limit(number: number): BinaryNumber;
+*/
